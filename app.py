88bafbde8e5b306a9172301459d834b91a92eb91
Robert Patterson <robert.patterson@cognosos.com>
2020-10-02 11:20:17 -0400

    Added status change functionality

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ from supporting_scripts.delimit_csv import extract_ids
 import pandas as pd
 import time
 import math
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 #####----Setting root page layout----#####
 root = Tk()
 root.title('Device App')
 root.iconbitmap('images/cognosos.ico')
-root.geometry("400x300")
+root.geometry("440x300")
 
 
 ################----Login Window----######################
@@ -57,7 +60,7 @@ user_lbl = Label(top, text="username")
 user_lbl.grid(row=0,column=0)
 user_entry = Entry(top, width=20, borderwidth=5)
 user_entry.grid(row=1,column=0)
-user_entry.insert(0, 'device_user')
+user_entry.insert(0, 'device_api_user')
 
 pw_lbl = Label(top, text="password")
 pw_lbl.grid(row=0,column=1)
@@ -102,10 +105,10 @@ def pickfile():
 
     e_1.insert(0, message)
 
+
+#Browse Button
 browse_button = Button(root, padx=50, pady=10, text="Browse", command=pickfile)
-browse_button.grid(row=2, column=1)
-
-
+browse_button.grid(row=1, column=1)
 
 
 ########-----Outputs-----######
@@ -167,7 +170,7 @@ def get_device_info():
         message = "Time remaining: " + str(math.floor(time_left/60)) + " minutes" + " " + str(time_left%60) + " seconds"
         label_5 = Label(root, text=message)
         label_5.grid_remove()
-        label_5.grid(row=7, column=0, columnspan=2)
+        label_5.grid(row=8, column=0, columnspan=2)
 
         #!#!-Always remember to use this when iterating or app will freeze until loop has ended-#!#!
         root.update_idletasks()
@@ -184,7 +187,7 @@ def get_device_info():
     #Output message "Done"
     label_5.grid_remove()
     label_5 = Label(root, padx=200, text="Done")
-    label_5.grid(row=7, column=0, columnspan=2)
+    label_5.grid(row=8, column=0, columnspan=2)
 
     #resize root window
     root.geometry('450x350')
@@ -193,28 +196,87 @@ def get_device_info():
         my_df.to_csv('~/Desktop/device_output.csv')
 
     export_button = Button(root, padx=50, pady=10, text='Export CSV to Desktop?', command=export)
-    export_button.grid(row=6,column=0, columnspan=2)
+    export_button.grid(row=7,column=0, columnspan=2)
 
+
+def change_entry_statuses():
+
+    def change_status(device, status_num):
+
+        BASE_URL = "https://api.cognosos.net/device/update/" + str(device)
+        HEADERS = {'Content-type': 'application/json'}
+        BODY = {"status":status_num}
+        requests.put(BASE_URL, headers=HEADERS, json=BODY, auth=HTTPBasicAuth(username, password))
+
+    #initialize progress bar
+    progress_bar['value'] = 0
+    
+    my_status_choice = clicked.get()
+    status_num = my_status_choice.split(" - ")[0]
+
+    my_devs = e_1.get()
+    dev_list = my_devs.split(", ")
+
+    count = 0
+    t0 = time.time()
+
+    for device in dev_list:
+
+        change_status(device, status_num)
+
+        #update progress bar
+        progress_bar['value'] += 100*(1/(len(dev_list)))
+        
+        ###--Time Remaining--###
+        #find average time elapsed per device so far
+        count += 1
+        t1 = time.time()
+        time_passed = t1-t0
+        per_device = time_passed/count
+
+        #use this to extrapolate time remaining
+        remaining_devices = len(dev_list)-count
+        time_left = round(remaining_devices*per_device,0)
+
+        #Create output message
+        message = "Time remaining: " + str(math.floor(time_left/60)) + " minutes" + " " + str(time_left%60) + " seconds"
+        label_5 = Label(root, text=message)
+        label_5.grid_remove()
+        label_5.grid(row=8, column=0, columnspan=2)
+
+        root.update_idletasks()
 
 ####---Root Layout---####
 
 #Get Info Button
 button_1 = Button(root, padx=50, pady=20, text="Get Info", command=get_device_info)
-button_1.grid(row=2, column=0, columnspan=1)
+button_1.grid(row=4, column=0, columnspan=1)
+
+#Change Status Button
+button_1 = Button(root, padx=50, pady=20, text="Change Status", command=change_entry_statuses)
+button_1.grid(row=4, column=1, columnspan=1)
+
+#Dropdown Menu for Status Change Option
+clicked = StringVar()
+clicked.set("10 - Passed All")
+drop = OptionMenu(root, clicked, "10 - Passed All", "16 - Return to Cognosos", "0 - No Unit Test, No Board Test", "1 - No Unit Test, Failed Board Test", "2 - No Unit Test, Passed Board Test", "5 - Failed Unit Test, Failed Board Test", "6 - Failed Unit Test, Passed Board Test", "9 - Passed Unit Test, Failed Board Test")
+drop.grid(row=3, column=1)
 
 #Progress Bar
 progress_bar = ttk.Progressbar(root, orient=HORIZONTAL, length=200, mode='determinate')
-progress_bar.grid(row=5, column=0, columnspan=2)
+progress_bar.grid(row=6, column=0, columnspan=2)
 
 #Exit Button
 exit_button = Button(root, padx = 50, pady=20, text="Exit Program", command=root.quit)
-exit_button.grid(row=8, column=0, columnspan=2)
+exit_button.grid(row=9, column=0, columnspan=2)
 
 #Spacing
 label_3 = Label(root, text = '')
-label_3.grid(row=4, column=0, columnspan=2)
-
+label_3.grid(row=5, column=0, columnspan=2)
 label_4 = Label(root, text = '')
-label_4.grid(row=6, column=0, columnspan=2)
+label_4.grid(row=7, column=0, columnspan=2)
+
+label_5 = Label(root, text = '')
+label_5.grid(row=3, column=0, columnspan=2)
 
 root.mainloop()
